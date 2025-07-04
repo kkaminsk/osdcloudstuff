f680a6c336a5c8eb7ad6e0cc3c4db3f173c10fc9
Kevin Kaminski MVP <Kevin.Kaminski@bighatgroup.com>
2025-07-04 17:52:42 -0600

    Let's see if any of this works.

---
+++ 3template.ps1
@@ -0,0 +1,16 @@
+#Set my working OSDCloud Template 
+
+Set-OSDCloudTemplate -Name 'TestOSD'
+New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud\TestOSD
+Set-OSDCloudWorkspace C:\OSDCloud\TestOSD
+
+#Cleanup Languages 
+
+$KeepTheseDirs = @('boot','efi','en-us','sources','fonts','resources') 
+
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force 
+
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force 
+
+Get-ChildItem "$(Get-OSDCloudWorkspace)\Media\EFI\Microsoft\Boot" | Where {$_.PSIsContainer} | Where {$_.Name -notin $KeepTheseDirs} | Remove-Item -Recurse -Force 
+
---
+++ 4finalstep.ps1
@@ -0,0 +1,1 @@
+Edit-OSDCloudWinPE -PSModuleCopy -OSD -StartOSDCloudGUI
