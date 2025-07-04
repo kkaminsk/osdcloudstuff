4afdedef324a351f043cb613d07d482cf441aeec
Kevin Kaminski MVP <Kevin.Kaminski@bighatgroup.com>
2025-07-04 17:15:05 -0600

    Add getting the JSON

---
+++ 2getconfiguration.ps1
@@ -0,0 +1,5 @@
+# Use the following command to start the GUI
+Start-OSDCloudGUI
+
+# Close the GUI and look for the JSON file. Below is an example.
+# C:\Users\ADMINI~1\AppData\Local\Temp\Start-OSDCloudGUI.json
