8a64dd895e2e3679267dccce7a28a6df8c0b7fbd
Kevin Kaminski MVP <Kevin.Kaminski@bighatgroup.com>
2025-07-17 17:36:33 -0600

    File references

--- startnet.cmd
+++ startnet.cmd
@@ -6,8 +6,8 @@ PowerShell -Nol -C Initialize-OSDCloudStartnet
 PowerShell -Nol -C Initialize-OSDCloudStartnetUpdate
 @ECHO OFF
 ECHO Start-OSDCloud
-start /wait PowerShell -NoL -W Ma -C x:\OSDCustom\custom1-start.ps1
+start /wait PowerShell -NoL -W Ma -file x:\OSDCustom\custom1-start.ps1
 start /wait PowerShell -NoL -W Ma -C Start-OSDCloud -ImageFileUrl "http://PFCOMDC1/install.wim" -imageindex 3
-start /wait PowerShell -NoL -W Mi -C x:\OSDCustom\custom2-end.ps1
+start /wait PowerShell -NoL -W Mi -file x:\OSDCustom\custom2-end.ps1
 start /wait PowerShell -NoL -W Mi -C restart-computer -force
 @ECHO ON
